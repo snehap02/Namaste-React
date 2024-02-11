68913e48ac1a219511a3c3a9ffe505024e0370ef
Sneha Purkayastha <iamsneha0296@gmail.com>
2024-02-11 07:51:13 +0530

    new changes

--- Episode4-Talk is Cheap, Show Me The Code/Coding/App.js
+++ Episode4-Talk is Cheap, Show Me The Code/Coding/App.js
@@ -20,16 +20,140 @@ const Header = () => {
 }
 
 const RestaurantCard = (props) => {
+    // const {resName, cuisine} = props;
+    const {resData} = props;
     return (
         <div className="res-card" style={{backgroundColor: "#f0f0f0"}}>
             <img src="https://cookingfromheart.com/wp-content/uploads/2017/09/Veg-Biryani-in-Pressure-Cooker-6.jpg" alt="res-logo" />
-            <h3>{props.resName}</h3>
-            <h4>{props.cuisine}</h4>
-            <h4>⭐4.4</h4>
-            <h4>38 Minutes</h4>
+            <h3>{resData.data.name}</h3>
+            <h4>{resData.data.cuisines.join(" , ")}</h4>
+            <h4>⭐{resData.data.avgRating}</h4>
+            <h4>{resData.data.slaString}</h4  >
         </div>
     )
 }
+
+const resObj = {
+      type: "restaurant",
+      data: {
+        type: "F",
+        id: "74453",
+        name: "Domino's Pizza",
+        uuid: "87727dbd-7f2b-4857-9763-359624165845",
+        city: "21",
+        area: "Athwa",
+        totalRatingsString: "1000+ ratings",
+        cloudinaryImageId: "bz9zkh2aqywjhpankb07",
+        cuisines: ["Pizzas", "Fast Food", "Snacks", "American"],
+        tags: [],
+        costForTwo: 40000,
+        costForTwoString: "₹400 FOR TWO",
+        deliveryTime: 45,
+        minDeliveryTime: 45,
+        maxDeliveryTime: 45,
+        slaString: "45 MINS",
+        lastMileTravel: 0,
+        slugs: {
+          restaurant: "dominos-pizza-majura-nondh-test_surat",
+          city: "surat",
+        },
+        cityState: "21",
+        address:
+          "Shop 32 To 35, Sheetal Shopping Square,Near Lb Turning Point, Bhatar Road,MAJURA NONDH, Surat,GUJARAT-395001",
+        locality: "Bhatar Road",
+        parentId: 2456,
+        unserviceable: false,
+        veg: true,
+        select: false,
+        favorite: false,
+        tradeCampaignHeaders: [],
+        aggregatedDiscountInfo: {
+          header: "FREE DELIVERY",
+          shortDescriptionList: [
+            {
+              meta: "FREE DELIVERY",
+              discountType: "FREE_DELIVERY",
+              operationType: "RESTAURANT",
+            },
+          ],
+          descriptionList: [
+            {
+              meta: "FREE DELIVERY",
+              discountType: "FREE_DELIVERY",
+              operationType: "RESTAURANT",
+            },
+          ],
+          subHeader: "",
+          headerType: 0,
+          superFreedel: "",
+        },
+        aggregatedDiscountInfoV2: {
+          header: "",
+          shortDescriptionList: [
+            {
+              meta: "Free Delivery",
+              discountType: "FREE_DELIVERY",
+              operationType: "RESTAURANT",
+            },
+          ],
+          descriptionList: [
+            {
+              meta: "FREE DELIVERY",
+              discountType: "FREE_DELIVERY",
+              operationType: "RESTAURANT",
+            },
+          ],
+          subHeader: "",
+          headerType: 0,
+          superFreedel: "",
+        },
+        chain: [],
+        feeDetails: {
+          fees: [],
+          totalFees: 0,
+          message: "",
+          title: "",
+          amount: "",
+          icon: "",
+        },
+        availability: {
+          opened: true,
+          nextOpenMessage: "",
+          nextCloseMessage: "",
+        },
+        longDistanceEnabled: 0,
+        rainMode: "NONE",
+        thirdPartyAddress: false,
+        thirdPartyVendor: "",
+        adTrackingID: "",
+        badges: {
+          imageBased: [],
+          textBased: [],
+          textExtendedBadges: [],
+        },
+        lastMileTravelString: "2.1 kms",
+        hasSurge: false,
+        sla: {
+          restaurantId: "74453",
+          deliveryTime: 45,
+          minDeliveryTime: 45,
+          maxDeliveryTime: 45,
+          lastMileTravel: 0,
+          lastMileDistance: 0,
+          serviceability: "SERVICEABLE",
+          rainMode: "NONE",
+          longDistance: "NOT_LONG_DISTANCE",
+          preferentialService: false,
+          iconType: "EMPTY",
+      },
+      promoted: false,
+      avgRating: "3.9",
+      totalRatings: 0,
+      new: true,
+    },
+    subtype: "basic",
+  };
+  
 
 const Body = () => {
     return (
@@ -38,8 +162,7 @@ const Body = () => {
                 Search
             </div>
             <div className="res-container">
-                <RestaurantCard resName="Meghana Foods" cuisine="Biryani, North Indian, Asian"/>
-                <RestaurantCard resName="KFC" cuisine="Fast Food"/>
+                <RestaurantCard resData = {resObj}/>
             </div>
         </div>
     )
