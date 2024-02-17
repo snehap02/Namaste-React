3bab044ce3de0a15f986129954d6a994c6333b3b
Sneha Purkayastha <iamsneha0296@gmail.com>
2024-02-17 22:48:54 +0530

    made some changes

--- Episode6-Exploring the World/Coding/src/components/Body.js
+++ Episode6-Exploring the World/Coding/src/components/Body.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
 import { useState } from "react";
@@ -6,7 +6,19 @@ import { useState } from "react";
 const Body = () => {
 
   const [listOfRestaurents, setListOfRestaurents] = useState(resList);
-  
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING');
+
+    const json = await data.json();
+    console.log(json);
+    // setListOfRestaurents(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+  }
+
   return (
     <div className="body">
       {/* <div className="search">Search</div> */}
