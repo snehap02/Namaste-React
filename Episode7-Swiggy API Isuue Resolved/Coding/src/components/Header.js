ffa2b7643722f3345ceb47884603dd75877b6bdf
Sneha Purkayastha <iamsneha0296@gmail.com>
2024-02-20 10:15:04 +0530

    files added

---
+++ Episode7-Swiggy API Isuue Resolved/Coding/src/components/Header.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from "react";
+import { LOGO_URL } from "../utils/constants";
+
+const Header = () => {
+  const [login, setLoginToLogout] = useState("Login");
+
+  // useEffect(() => {
+  //   setLoginToLogout("Login");
+  // }, []);
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          src={ LOGO_URL}
+          alt=""
+          className="logo"
+        />
+      </div>
+      <div className="nav-links">
+        <ul className="links">
+          <li>Home</li>
+          <li>About US</li>
+          <li>Contact US</li>
+          <li>Cart</li>
+          <button className="login" onClick={
+            () => {
+              login === 'Login' ? setLoginToLogout("Logout") : setLoginToLogout('Login')
+              
+            }
+          }>{login}</button>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
