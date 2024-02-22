9085c09666be983f47eb50a444c3ba5a131acbf6
Sneha Purkayastha <iamsneha0296@gmail.com>
2024-02-22 21:43:21 +0530

    more components added

--- Episode7-Finding The Path/Coding/src/App.js
+++ Episode7-Finding The Path/Coding/src/App.js
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const AppContainer = () => {
   return (
@@ -12,5 +15,20 @@ const AppContainer = () => {
   );
 };
 
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppContainer />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
+  },
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppContainer />); 
+root.render(<RouterProvider router={appRouter} />);
