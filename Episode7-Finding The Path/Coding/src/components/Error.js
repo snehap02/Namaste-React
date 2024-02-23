d20d53882146e85e14f8e25a233040049ee5d20d
Sneha Purkayastha <iamsneha0296@gmail.com>
2024-02-23 18:22:32 +0530

    changes in the code

--- Episode7-Finding The Path/Coding/src/components/Error.js
+++ Episode7-Finding The Path/Coding/src/components/Error.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import { useRouteError } from 'react-router-dom'
 
 const Error = () => {
+
+    const error = useRouteError(); 
+    console.log(error);
+
   return (
     <div>
-        <h1>Oops</h1>
+        <h1>Oops!! </h1>
         <h2>Something Went Wrong!!</h2>
+        <h3>{error.status}: {error.statusText} </h3>
     </div>
   )
 }
