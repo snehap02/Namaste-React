6981d8be1a87395a3559f06689cab8570e4e5249
Sneha Purkayastha <iamsneha0296@gmail.com>
2024-02-23 13:53:06 +0530

    creating our own error page

--- Episode7-Finding The Path/Coding/src/App.js
+++ Episode7-Finding The Path/Coding/src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import Body from "./components/Body";
 import About from "./components/About";
 import Contact from "./components/Contact";
+import Error from "./components/Error";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const AppContainer = () => {
@@ -19,6 +20,7 @@ const appRouter = createBrowserRouter([
   {
     path: "/",
     element: <AppContainer />,
+    errorElement: <Error/>,
   },
   {
     path: "/about",
---
+++ Episode7-Finding The Path/Coding/src/components/Error.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Error = () => {
+  return (
+    <div>
+        <h1>Oops</h1>
+        <h2>Something Went Wrong!!</h2>
+    </div>
+  )
+}
+
+export default Error
