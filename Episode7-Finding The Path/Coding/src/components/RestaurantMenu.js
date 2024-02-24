53c7cb2f0d2b021002f61d2a1a3f4e53cbad7d6b
Sneha Purkayastha <iamsneha0296@gmail.com>
2024-02-24 21:44:32 +0530

    new changes

--- Episode7-Finding The Path/Coding/src/components/RestaurantMenu.js
+++ Episode7-Finding The Path/Coding/src/components/RestaurantMenu.js
@@ -1,25 +1,46 @@
 import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
 
 const RestaurantMenu = () => {
+  const [resInfo, setResInfo] = useState(null);
+
   useEffect(() => {
     fetchMenu();
   }, []);
 
   const fetchMenu = async () => {
     const data = await fetch(
-      "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=26.16363&lng=91.7611838&restaurantId=66963&catalog_qa=undefined&submitAction=ENTER"
+      "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=26.16363&lng=91.7611838&restaurantId=791635&catalog_qa=undefined&submitAction=ENTER"
     );
     const json = await data.json();
     console.log(json);
+    setResInfo(json.data);
   };
+
+  // 🟡 if we first try to find names and all the properties from resInfo without checking whether its value is null or not react will not able to find any properties inside it, so first check the value of resInfo before finding any value
+
+  // if resInfo is null then do this
+  if (resInfo === null) return <Shimmer />;
+
+  // if not null then do this
+  const { name, cuisines, costForTwoMessage } =
+    resInfo?.cards[2]?.card?.card?.info;
+
+  const { itemCards } =
+    resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card;
+  console.log(itemCards);
+
   return (
     <div className="menu">
-      <h1 className="name">Name Of The Restaurant</h1>
-      <h2>Menu</h2>
+      <h1>{name}</h1>
+      <h2>{cuisines}</h2>
+      <h2>{costForTwoMessage}</h2>
+      <hr></hr>
+      <h1>Menus :-------</h1>
       <ul>
-        <li className="menu1">Menu 1</li>
-        <li className="menu2">Menu 2</li>
-        <li className="menu3">Menu 3</li>
+        {itemCards.map((item) => (
+          <li>{item.card.info.name} -- Rs {item.card.info.defaultPrice/100 || item.card.info.price/100}</li>
+        ))}
       </ul>
     </div>
   );
