4023d83cbeb26ff82b03f18ad8312a8528f10d14
Sneha Purkayastha <iamsneha0296@gmail.com>
2024-03-07 18:14:44 +0530

    new changes

--- Episode8-Lets Get Classy/Coding/src/components/About.js
+++ Episode8-Lets Get Classy/Coding/src/components/About.js
@@ -1,15 +1,31 @@
-import User from "./User";
+import React from "react";
 import UserClass from "./UserClass";
 
-const About = () => {
-  return (
-    <div>
-      <h1>About Us Page</h1>
-      {/* <User name={"Sneha Purkayastha (is using functional component)"}/> */}
+class About extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>About Class Based Component</h1>
 
-      <UserClass name={"Sneha Purkayastha (is using Class based component)"}/>
-    </div>
-  )
+        <UserClass
+          name={"Sneha Purkayastha (is using Class based component)"}
+        />
+      </div>
+    );
+  }
 }
+
+// const About = () => {
+//   return (
+//     <div>
+//       <h1>About Us Page</h1>
+
+//       <UserClass name={"Sneha Purkayastha (is using Class based component)"}/>
+//     </div>
+//   )
+// }
 
 export default About;
